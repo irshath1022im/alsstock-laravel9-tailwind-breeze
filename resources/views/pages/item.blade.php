4f48ea467dbf149f6b95f234ca54bf6d78183483
irshath1022im <im.irshath1022@gmail.com>
2022-11-23 14:42:57 +0300

    item page day2

--- resources/views/pages/item.blade.php
+++ resources/views/pages/item.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="border border-slate-700 rounded">
+    <div  class="border border-slate-700 rounded">
         <h5 class="bg-slate-500 p-2 text-2xl">ITEM : <span class="bg-blue-200  px-2 mx-3 rounded-full text-lg">{{ $item->id }}</span></h5>
 
         <div class="grid grid-cols-12">
@@ -27,52 +27,97 @@
 
             {{-- ITEMS SIZE COLUMN --}}
 
-            <div class=" col-span-12 sm:col-span-9 m-2 rounded-lg border flex flex-col items-center bg-slate-100">
+            <div class=" col-span-12 sm:col-span-9 m-2 rounded-lg border flex flex-col items-center bg-slate-100"
+                x-data="{
+                    active_id : null ,
+                    setActiveId(value){
+                        this.active_id = value
+                    },
+                    closeAll(){
+                        this.active_id = null
+                    }
+                }"
+            >
 
                 @foreach ($item->itemSize as $item)
 
+                <div x-data="{open:false}">
 
 
-                <div class="mb-1">
+                    <div class="mb-1 flex"  >
 
-                    <button type="button" class="inline-flex items-center px-5 py-2.5 text-small font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-blue-300">
-                      {{ $item->id}} {{ $item->size->size }}
-                        <span class=" inline-flex  justify-center ml-2 w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
-                           {{  $item->transectionLogs->sum('qty') }}
+                        <button   @click="setActiveId({{ $item->id }})" id="{{ $item->id }}" type="button" class="inline-flex items-center px-5 py-2.5 text-small font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-blue-300"   :aria-expanded="open">
+                        {{ $item->id}} {{ $item->size->size }}
+                            <span class=" inline-flex  justify-center ml-2 w-4 h-4 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
+                            {{  $item->transectionLogs->sum('qty') }}
+                            </span>
+
+
+                        {{-- <span x-show="!open" class="border border-red-900 bg-slate-300 rounded" >
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                            </svg>
                         </span>
-                    </button>
+
+                            <svg x-show = "open" class="border border-red-900 bg-slate-300 rounded" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                            </svg> --}}
+
+
+                        </button>
+
+
+
+                    </div>
+
+
+                    <div x-show="active_id == {{ $item->id }} ? true : false" x-transition.duration.500ms>
+
+                        <button @click="closeAll()" x-show="active_id == {{ $item->id }} ?? true" class="flex justify-end">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                              </svg>
+                        </button>
+
+                        <table class="table-auto   border-separate border  border-slate-400">
+                            <thead>
+
+
+
+                                <tr class="">
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">TRANS ID</th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">ITEM SIZE ID</th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">TRANS TYPE</th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">QTY</th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">REMARK</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+
+                                @foreach ($item->transectionLogs as $log)
+
+
+                                    <tr class="bg-gray-100 border-b">
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $log->id }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $log->size }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $log->transection_type }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $log->qty }}</td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $log->remark }}</td>
+                                    </tr>
+
+                                @endforeach
+
+                            </tbody>
+
+
+                        </table>
+
+
+                    </div>
+
                 </div>
 
-                <table class="table-auto   border-separate border  border-slate-400">
-                    <thead>
-                        <tr class="">
-                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">TRANS ID</th>
-                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">ITEM SIZE ID</th>
-                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">TRANS TYPE</th>
-                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">QTY</th>
-                            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">REMARK</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-
-                        @foreach ($item->transectionLogs as $log)
-
-
-                            <tr class="bg-gray-100 border-b">
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $log->id }}</td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $log->size }}</td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $log->transection_type }}</td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $log->qty }}</td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $log->remark }}</td>
-                            </tr>
-
-                        @endforeach
-
-                    </tbody>
-
-
-                </table>
 
 
                 @endforeach
