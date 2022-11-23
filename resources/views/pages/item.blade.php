0cfcd22da8313d43949a3e3eff9c7c90484c2ab0
irshath1022im <im.irshath1022@gmail.com>
2022-11-23 15:38:08 +0300

    item page day2.1

--- resources/views/pages/item.blade.php
+++ resources/views/pages/item.blade.php
@@ -73,11 +73,14 @@
 
                     <div x-show="active_id == {{ $item->id }} ? true : false" x-transition.duration.500ms>
 
-                        <button @click="closeAll()" x-show="active_id == {{ $item->id }} ?? true" class="flex justify-end">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                              </svg>
-                        </button>
+                        <div class="relative">
+
+                            <button @click="closeAll()" x-show="active_id == {{ $item->id }} ?? true">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 absolute top-0 right-0">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                  </svg>
+                            </button>
+                        </div>
 
                         <table class="table-auto   border-separate border  border-slate-400">
                             <thead>
