30ab7297f60081d9ef4c3f9545586a6a86e24003
irshath1022im <im.irshath1022@gmail.com>
2023-02-26 14:55:02 +0300

    home page query chagned

--- app/Http/Livewire/Components/ItemList.php
+++ app/Http/Livewire/Components/ItemList.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Components;
 
+use App\Models\Category;
 use App\Models\Item;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -12,21 +13,45 @@ class ItemList extends Component
     use WithPagination;
 
     public $selected_category = 1;
+    public $selected_store = 1;
+    public $categories=[];
 
 
-    protected $listeners=['selectedCategory'];
+    protected $listeners=['updateSelectedStore' => 'selectedStoreItemRequest', 'selectedCategory'];
+
 
     public function selectedCategory($id)
     {
         $this->resetPage();
-        $this->selected_category = $id;
+        $this->categories = [$id];
+    }
+
+    public function selectedStoreItemRequest($store_id)
+    {
+        $this->selected_store = $store_id;
+        $this->getCategoriesForStore();
+    }
+
+    public function getCategoriesForStore()
+    {
+        $this->resetPage();
+      $categories = Category::where('store_id', $this->selected_store)->get();
+        $this->categories = $categories->pluck('id');
+    }
+
+    public function mount()
+    {
+        $this->getCategoriesForStore();
     }
 
 
 
     public function render()
     {
-        $result = Item::where('category_id', $this->selected_category)->with('category')->paginate(9);
+        // $result = Item::where('category_id', $this->selected_category)->with('category')->paginate(9);
+
+        // $result = Item::whereIn('category_id', $this->categories)->paginte(5);
+            $result = Item::with('category')->whereIn('category_id',$this->categories)->paginate(8);
         return view('livewire.components.item-list',['items' => $result]);
     }
 }
