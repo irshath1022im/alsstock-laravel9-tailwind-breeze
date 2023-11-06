42904701465053e9bb61d5a45c35943d6d4fb0e7
irshath1022im <im.irshath1022@gmail.com>
2023-11-06 10:11:42 +0300

    items pages resolved

--- resources/views/pages/item/index.blade.php
+++ resources/views/pages/item/index.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 
 
-<h4>Loged As a : <span class="border px-2 py-1 bg-green-400 rounded-md text-white uppercase">{{ Auth::user()->name }}</span></h4>
+
 
 @can('isAdmin')
 
-    <h4>I am Admin</h4>
+    <h4>Loged As a : <span class="border px-2 py-1 bg-green-400 rounded-md text-white uppercase">{{ Auth::user()->name }}</span></h4>
 
     @else
 
-    <h4>I am not Admin</h4>
+   <h4>Loged As a : <span class="border px-2 py-1 bg-green-400 rounded-md text-white uppercase">Guest</span></h4>
 @endcan
 
 {{-- <h4>Loged As a : <span class="border px-2 py-1 bg-green-400 rounded-md text-white uppercase">{{ Auth::user()->name }}</span></h4>
