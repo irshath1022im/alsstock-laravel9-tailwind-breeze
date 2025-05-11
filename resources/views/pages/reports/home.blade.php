8c781ee1e60c6eb3a76ed1f12da57a5fbe146ea0
irshath1022im <im.irshath1022@gmail.com>
2025-05-11 13:13:53 +0300

    homePage correction

--- resources/views/pages/reports/home.blade.php
+++ resources/views/pages/reports/home.blade.php
@@ -11,8 +11,8 @@
 
         <div class="card-body">
             <div class="p-3 border-b "><a href ="{{ route('uniformReports') }}" target="_blank" >UNIFORMS REPORT</a></div>
-            <div class="p-3 border-b"><a href="{{ route('promotionalItemsReports') }}"target="_blank" >PROMOTIONAL REPORT</a></div>
-            <div class="p-3 border-b"><a href="{{ route('consumptionPage') }}" target="_blank">CONUMPTION REPORTS</a></div>
+            <div class="p-3 border-b"><a href="{{ route('promotionalItemsReports') }}"target="_blank" >PROMOTIONAL ITEMS REPORT</a></div>
+            <div class="p-3 border-b"><a href="{{ route('consumptionPage') }}" target="_blank">CONSUMPTION REPORTS</a></div>
             <div class="p-3 border-b"><a href="{{ route('reportSummary') }}" target="_blank">SUMMARY</a></div>
         </div>
     </div>
